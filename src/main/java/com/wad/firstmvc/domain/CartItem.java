b7691bdff67d7e2b49cd272b681e5a0d1b9bd38f
AnutaTeo <amam.teodorescu@gmail.com>
2025-05-18 15:18:45 +0300

    Added cart page

---
+++ src/main/java/com/wad/firstmvc/domain/CartItem.java
@@ -0,0 +1,20 @@
+package com.wad.firstmvc.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class CartItem {
+    private Crystal product;
+    @Setter
+    private int quantity;
+
+    public CartItem(Crystal product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public double getSubtotal() {
+        return product.getPrice() * quantity;
+    }
+}
