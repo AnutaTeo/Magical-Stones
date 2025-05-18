5da14036d4d6f2ce1d55c11953844ed6823601c2
msrgq <msurugiu1301@gmail.com>
2025-05-19 00:30:34 +0300

    Added admin-panel

--- src/main/java/com/wad/firstmvc/controllers/ZodiacController.java
+++ src/main/java/com/wad/firstmvc/controllers/ZodiacController.java
@@ -22,9 +22,53 @@ public class ZodiacController {
     private String matchCrystal(String sign, String energy) {
         //mai punem chestii personalizate daca e pe viitor
         if ("Aries".equals(sign) && "Protection".equals(energy)) return "Red Jasper";
-        if ("Cancer".equals(sign)) return "Moonstone";
-        if ("Gemini".equals(sign)) return "Agate";
-        if ("Pisces".equals(sign)) return "Amethyst";
+        if ("Aries".equals(sign) && "Love".equals(energy)) return "Rhodonite";
+        if ("Aries".equals(sign) && "Creativity".equals(energy)) return "Carnelian";
+
+        if ("Taurus".equals(sign) && "Protection".equals(energy)) return "Smoky Quartz";
+        if ("Taurus".equals(sign) && "Love".equals(energy)) return "Rose Quartz";
+        if ("Taurus".equals(sign) && "Creativity".equals(energy)) return "Aventurine";
+
+        if ("Gemini".equals(sign) && "Protection".equals(energy)) return "Labradonite";
+        if ("Gemini".equals(sign) && "Love".equals(energy)) return "Chrysoprase";
+        if ("Gemini".equals(sign) && "Creativity".equals(energy)) return "Citrine";
+
+        if ("Cancer".equals(sign) && "Protection".equals(energy)) return "Moonstone";
+        if ("Cancer".equals(sign) && "Love".equals(energy)) return "Rhodocrosite";
+        if ("Cancer".equals(sign) && "Creativity".equals(energy)) return "Aquamarine";
+
+        if ("Leo".equals(sign) && "Protection".equals(energy)) return "Tiger's Eye";
+        if ("Leo".equals(sign) && "Love".equals(energy)) return "Garnet";
+        if ("Leo".equals(sign) && "Creativity".equals(energy)) return "Sunstone";
+
+        if ("Virgo".equals(sign) && "Protection".equals(energy)) return "Fluorite";
+        if ("Virgo".equals(sign) && "Love".equals(energy)) return "Pink Opal";
+        if ("Virgo".equals(sign) && "Creativity".equals(energy)) return "Amzonite";
+
+        if ("Libra".equals(sign) && "Protection".equals(energy)) return "Obsidian";
+        if ("Libra".equals(sign) && "Love".equals(energy)) return "Rose Quartz";
+        if ("Libra".equals(sign) && "Creativity".equals(energy)) return "Lapis Lazuli";
+
+        if ("Scorpio".equals(sign) && "Protection".equals(energy)) return "Obsidian";
+        if ("Scorpio".equals(sign) && "Love".equals(energy)) return "Rhodonite";
+        if ("Scorpio".equals(sign) && "Creativity".equals(energy)) return "Garnet";
+
+        if ("Sagittarius".equals(sign) && "Protection".equals(energy)) return "Jet";
+        if ("Sagittarius".equals(sign) && "Love".equals(energy)) return "Tourmaline";
+        if ("Sagittarius".equals(sign) && "Creativity".equals(energy)) return "Turquoise";
+
+        if ("Capricorn".equals(sign) && "Protection".equals(energy)) return "Onyx";
+        if ("Capricorn".equals(sign) && "Love".equals(energy)) return "Ruby";
+        if ("Capricorn".equals(sign) && "Creativity".equals(energy)) return "Garnet";
+
+        if ("Aquarius".equals(sign) && "Protection".equals(energy)) return "Hematite";
+        if ("Aquarius".equals(sign) && "Love".equals(energy)) return "Kunzite";
+        if ("Aquarius".equals(sign) && "Creativity".equals(energy)) return "Amethyst";
+
+        if ("Pisces".equals(sign) && "Protection".equals(energy)) return "Tourmaline";
+        if ("Pisces".equals(sign) && "Love".equals(energy)) return "Rose Quartz";
+        if ("Pisces".equals(sign) && "Creativity".equals(energy)) return "Moonstone";
+
         return "Clear Quartz";
     }
 }
