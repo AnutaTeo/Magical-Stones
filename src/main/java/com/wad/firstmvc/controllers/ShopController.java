6baf85ad51cffcc30ad13c546252f17fe3197c7c
AnutaTeo <amam.teodorescu@gmail.com>
2025-05-19 20:48:01 +0300

    Added color filtering

--- src/main/java/com/wad/firstmvc/controllers/ShopController.java
+++ src/main/java/com/wad/firstmvc/controllers/ShopController.java
@@ -1,9 +1,13 @@
 package com.wad.firstmvc.controllers;
 
+import com.wad.firstmvc.domain.Crystal;
 import com.wad.firstmvc.services.CrystalData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class ShopController {
@@ -14,8 +18,16 @@ public class ShopController {
     }
 
     @GetMapping("/shop")
-    public String shopPage(Model model) {
-        model.addAttribute("crystals", data.findAll());
+    public String shop(@RequestParam(required = false) String color, Model model) {
+        List<Crystal> crystals = data.findAll();
+
+        if (color != null && !color.isEmpty()) {
+            crystals = crystals.stream()
+                    .filter(c -> c.getColor().equalsIgnoreCase(color))
+                    .toList();
+        }
+
+        model.addAttribute("crystals", crystals);
         return "shop";
     }
 }
